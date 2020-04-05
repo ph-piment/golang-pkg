ff48bb1908944c5ac7ae971bc4d7a15c34648154
ph piment <ph.piment@gmail.com>
2020-04-05 23:26:28 +0900

    [update]use dotenv

--- redis/client.go
+++ redis/client.go
@@ -1,20 +1,60 @@
 package redis
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/go-redis/redis"
+	"github.com/joho/godotenv"
 )
 
 var redisClient *redis.Client
+var redisAddr string
+var redisPassword string
+var redisDb int
 
 // GetClient get redis client.
 func GetClient() *redis.Client {
 	if redisClient == nil {
+		_ = initEnv()
+
 		redisClient = redis.NewClient(&redis.Options{
-			Addr:     "redis:6379",
-			Password: "", // no password set
-			DB:       0,  // use default DB
+			Addr:     redisAddr,
+			Password: redisPassword,
+			DB:       redisDb,
 		})
 	}
 
 	return redisClient
+}
+
+func loadEnv() error {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getDb() (int, error) {
+	db, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if err != nil {
+		return 0, err
+	}
+	return db, nil
+}
+
+func initEnv() error {
+	err := loadEnv()
+	if err != nil {
+		return err
+	}
+
+	redisAddr = os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT")
+	redisPassword = os.Getenv("REDIS_PASSWORD")
+	redisDb, err = getDb()
+	if err != nil {
+		return err
+	}
+	return nil
 }
